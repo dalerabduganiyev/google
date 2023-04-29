f046813cf8d11be1ea29ca5f161d081ee1fe5d4f
dalerabduganiyev <126411318+dalerabduganiyev@users.noreply.github.com>
2023-04-29 17:22:29 +0500

    my second work

---
+++ main.js
@@ -0,0 +1,16 @@
+var hidden = document.querySelector('.hidden');
+var div2 = document.querySelector('.div2');
+
+div2.addEventListener('click', function(){
+hidden.classList.toggle('block');
+}); 
+
+
+var div = document.querySelector('.div');
+var hidden2 = document.querySelector('.hidden2');
+
+
+
+div.addEventListener('click', function(){
+    hidden2.classList.toggle('bb')
+})
